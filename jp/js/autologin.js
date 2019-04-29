3a000099d75bc936a1a0a0db1476d3d50c208de7
nikolamircic <nm@sitecake.com>
2019-04-29 10:37:56 +0200

    jp removed, frameworks removed
    - jp version of the site removed due to no easy way to maintain it
    - resources section removed
    - links on pages corrected to reflect the change

--- jp/js/autologin.js
+++
@@ -1,9 +0,0 @@
-var interval = setInterval(function () {
-    if (window.sitecakeGlobals && sitecakeGlobals.editMode === false) {
-        var loginInput = document.getElementsByClassName("GOB4QEDCIJ")[0];
-        if(loginInput) {
-            document.getElementsByClassName("GOB4QEDCIJ")[0].value = "admin";
-            clearInterval(interval);
-        }
-    }
-}, 1000);
